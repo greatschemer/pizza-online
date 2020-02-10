8f557ba58694ca7361012b4cc214beeb3d41938f
Nikolay Kozlov <nikolay@velkomb.com>
2020-02-10 11:35:22 +0500

    Remove bilerplate

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, {useState} from 'react';
-import './App.css';
 import Knock from "./components/Knock/Knock";
 
 function App() {
     return (
         <div className="App">
-            <Knock/>
         </div>
     );
 }
