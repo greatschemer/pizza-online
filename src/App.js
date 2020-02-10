26bee12d6d89f9a2156afbffaf83944acad1bbbb
Nikolay Kozlov <nikolay@velkomb.com>
2020-02-10 16:28:21 +0500

    Рефакторинг App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components'
 import { Navbar } from "./Navbar/Navbar";
 import { Banner } from "./Banner/Banner";
 import { Menu } from "./Menu/Menu";
-
-const GlobalStyle = createGlobalStyle`
-    body {
-        margin: 0;
-        font-family: 'Open Sans', sans-serif;
-    } 
-    h1, h2, h3 {
-        font-family: 'Russo One', cursive;
-    }
-`
-
+import { GlobalStyle } from "./Styles/GlobalStyle";
 
 function App() {
     return (
