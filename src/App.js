f3680d8bbaa5a84f084cd49629ffeb1ac089dfa4
Nikolay Kozlov <nikolay@velkomb.com>
2020-02-10 12:07:28 +0500

    Добавлены шрифты, установлен styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, {useState} from 'react';
-import Knock from "./components/Knock/Knock";
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        font-family: 'Open Sans', sans-serif;
+    }
+    h1, h2, h3 {
+        font-family: 'Russo One', cursive;
+    }
+`
+
 
 function App() {
     return (
-        <div className="App">
-        </div>
+        <>
+            <h1>Привет, как дела?</h1>
+            <GlobalStyle/>
+        </>
     );
 }
 
