18e870a421bddfeb4328a574c5fc0d11c4228765
Nikolay Kozlov <nikolay@velkomb.com>
2020-02-11 16:18:17 +0500

    пофикшен список напитков

--- src/Styles/GlobalStyle.js
+++ src/Styles/GlobalStyle.js
@@ -4,10 +4,10 @@ export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0; 
     font-family: 'Open Sans', sans-serif;
-  }
+  } 
   
   h1, h2, h3 {
-    font-family: 'Righteous', cursive;
+    font-family: 'Russo One', cursive;
   }
   
 `;
