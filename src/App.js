11f0e0cf6dc79f840069ba8ef147761e5b073e17
Nikolay Kozlov <nikolay@velkomb.com>
2020-02-11 15:01:12 +0500

    фикс заголовка окна браузера + добавлен favicon

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { useState } from 'react';
-import { Navbar } from "./Navbar/Navbar";
-import { Banner } from "./Banner/Banner";
-import { Menu } from "./Menu/Menu";
-import { FoodDialog } from "./FoodDialog/FoodDialog";
-import { GlobalStyle } from "./Styles/GlobalStyle";
+import React, {useState} from 'react';
+import {Navbar} from "./Navbar/Navbar";
+import {Banner} from "./Banner/Banner";
+import {Menu} from "./Menu/Menu";
+import {FoodDialog} from "./FoodDialog/FoodDialog";
+import {Order} from "./Order/Order";
+import {GlobalStyle} from "./Styles/GlobalStyle";
 
-import { useOpenFood } from "./Hooks/useOpenFood";
-import { useOrders } from "./Hooks/useOrders";
-import { useTitle } from "./Hooks/useTitle";
-
+import {useOpenFood} from "./Hooks/useOpenFood";
+import {useOrders} from "./Hooks/useOrders";
+import {useTitle} from "./Hooks/useTitle";
 
 
 function App() {
     const openFood = useOpenFood();
     const orders = useOrders();
-    useTitle({ ...openFood, ...orders });
+    useTitle({...openFood, ...orders});
 
     return (
         <>
             <GlobalStyle/>
             <FoodDialog {...openFood} {...orders} />
             <Navbar/>
+            <Order {...orders} {...openFood} />
             <Banner/>
             <Menu {...openFood} />
         </>
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -12,12 +12,13 @@ const MenuStyled = styled.div`
 export function Menu({setOpenFood}) {
     return (
         <MenuStyled>
-            {Object.entries(foods).map(([sectionName, foods]) => (
-                <>
+            {Object.entries(foods).map(([sectionName, foods], index) => (
+                <div key={index}>
                     <h1> {sectionName} </h1>
                     <FoodGrid>
-                        {foods.map(food => (
+                        {foods.map((food, i) => (
                             <Food
+                                key={i}
                                 img={food.img}
                                 onClick={() => {
                                     setOpenFood(food);
@@ -30,7 +31,7 @@ export function Menu({setOpenFood}) {
                             </Food>
                         ))}
                     </FoodGrid>
-                </>
+                </div>
             ))}
         </MenuStyled>
     );
